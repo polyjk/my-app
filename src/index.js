adf3f3b86e697a40c8ff0ce0be568a19874ccaf3
John <p5khamm@gmail.comm>
2021-03-16 17:51:48 -0500

    Updated Board's handleClick() to end, if winner.

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ import './index.css';
 
     handleClick(i){
         const squares = this.state.squares.slice();
+
+        if(calculateWinner(squares || squares[i]){
+            return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
